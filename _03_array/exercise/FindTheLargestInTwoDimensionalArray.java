6e0e5274e468fe5f96cfdf91b7157b7ed404eba3
huyhoangqn <lehuyhoangpass1@gmail.com>
2022-04-04 22:46:39 +0700

    UP

--- _03_array/exercise/FindTheLargestInTwoDimensionalArray.java
+++ _03_array/exercise/FindTheLargestInTwoDimensionalArray.java
@@ -6,39 +6,27 @@ public class FindTheLargestInTwoDimensionalArray {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter Row of array: ");
-        int row = Integer.parseInt(scanner.nextLine());
-        System.out.print("Enter Column of array: ");
-        int col = Integer.parseInt(scanner.nextLine());
+        System.out.print("Nhập số dòng : ");
+        int inputrow = scanner.nextInt();
+        System.out.print("Nhập số cột : ");
+        int inputcolum = scanner.nextInt();
+        System.out.print(maxNumber(inputrow,inputcolum));
 
-        double[][] arrays = new double[row][col];
-        for (int i = 0; i < row; i++) {
-            for (int j = 0; j < col; j++) {
-                System.out.print("Enter element "+(i+1)+"_"+(j+1)+" ");
-                arrays[i][j] = Double.parseDouble(scanner.nextLine());
-            }
-        }
-        System.out.println("Array elements:");
-        for (int i = 0; i < row; i++) {
-            for (int j = 0; j < col; j++){
-                System.out.print(arrays[i][j] + "\t");
-            }
-            System.out.println();
-        }
+    }
 
-        double max = 0;
-        int indexRow = 0;
-        int indexCol = 0;
-        for (int i = 0; i < row; i++) {
-            for (int j = 0; j < col; j++){
-                if (max < arrays[i][j]){
-                    max = arrays[i][j];
-                    indexRow = i +1;
-                    indexCol = j+1;
+    public static int maxNumber(int row, int colum ){
+        Scanner scanner = new Scanner(System.in);
+        int [][] array = new int[row][colum];
+        int max = array[0][0];
+        for (int i = 0; i < row ; i++) {
+            for (int j = 0; j < colum ; j++) {
+                System.out.print("array["+i+"]["+j+"] :"  );
+                array[i][j] = scanner.nextInt();
+                if (array[i][j] > max){
+                    max = array[i][j]  ;
                 }
             }
         }
-        System.out.println("Max value in Array is "+max+" at row "+ indexRow+ " colunm "+indexCol);
-
+        return max;
     }
 }
